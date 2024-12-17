8e7ce76fdf67d8bcbb37406d9cd46cdb53401f6b
STULLL <150591392+ShadoyB@users.noreply.github.com>
2024-12-17 22:13:10 +0500

    Add files via upload

---
+++ hernya222.py
@@ -0,0 +1,17 @@
+ddd = input('анектоды: ааа1, ффф2, ццц3:')
+while (ddd != 'off'):
+    if ddd == 'ааа1':
+        print('абаюдна')
+    elif ddd == 'ффф2':
+        print('у алины большие пяточки')
+    elif ddd == 'ццц3':
+        print('oaehfapdhfjakafkdsl')
+    else:
+        print("большие вкусные пяточки дрейка")
+    ddd = input('анектоды: ааа1, ффф2, ццц3')
+
+
+
+
+
+
